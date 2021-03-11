1dd66b23ac5f9944537d267219a8b051dd9749df
winston11 <winstonmcgriff27@gmail.com>
2021-03-11 15:29:34 -0500

    finisher

--- app/model/review.rb
+++ app/model/review.rb
@@ -1,21 +1,4 @@
 class Review < ActiveRecord::Base
     belongs_to :owner
     belongs_to :video_game
-
-    def add_rating_method
-        #rating = (1...5)
-        #if rating > 0
-            #rating == 1
-        #elsif rating > 1
-            #rating == 2
-        #elsif rating > 2
-            #rating == 3
-        #elsif rating > 3
-            #rating == 4
-            #elsif rating > 4
-                #rating == 5
-            #end
-            #rating
-        #end
-    end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -128,9 +128,9 @@ def genres(user)
     puts ""
     puts "IT'S ALL CAP!"
     puts ""
-    review_list = Review.where(video_game_id: game.id)
+    # review_list = Review.where(video_game_id: game.id)
     #game.reviews works as well because of ActiveRecord
-    review_list.each do |review|
+    game.reviews.each do |review|
         #binding.pry
         puts "#{review.owner.name} says: #{review.comment}"
         puts "score: #{review.score}"
